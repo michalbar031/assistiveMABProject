2a2f48d2d6e853c6f531152ed1fcb969dfe8a1af
michalbar031 <michalbar031@gmail.com>
2024-04-21 14:56:57 +0300

    rnn input concated

--- problem.py
+++ problem.py
@@ -2,33 +2,49 @@ from bandit import BetaBernoulliBandit
 from human import EpsilonGreedy
 from robot import Robot
 class Problem:
-    def __init__(self, n_arms, human_policy):
+    def __init__(self, n_arms, human_policy,n_rounds=1000):
         self.bandit = BetaBernoulliBandit(n_arms)
         self.human = human_policy(n_arms)
         self.robot = Robot(n_arms)
+        self.human_history_vector = [-1] * n_rounds
+        self.robot_history_vector = [-1] * n_rounds
+        self.human_policy_vector = self.get_policy_vector(self.human)
+        self.n_rounds = n_rounds
+        self.input_size = len(self.human_history_vector) + len(self.robot_history_vector)+len(self.human_policy_vector)
 
-    def run_round(self):
-        print("----round----")
+    def get_policy_vector(self, human_policy):
+        # Assuming there are 5 policies in total and human_policy is an instance of one of them
+        policy_vector = [0] * 5
+        # policy_names = [EpsilonGreedy, WSLS, ThompsonSampling, UCL, GittinsIndex]  # Add actual classes here
+        policy_names = ["EpsilonGreedy", "WSLS", "ThompsonSampling", "UCL", "GittinsIndex"]
+        policy_index = policy_names.index(type(human_policy))
+        policy_vector[policy_index] = 1
+        return policy_vector
+
+    def run_round(self, round_number):
+        # print("----round----")
+        #HUMAN
         human_choice = self.human.select_arm()
+        self.human_history_vector[round_number] = human_choice
         self.human.update_choices(human_choice)
-        print("human_choice:", human_choice)
-        print("human_choices:", self.human.choices)
         self.robot.update_human_choice(human_choice)
-
+        #ROBOT
         robot_choice = self.robot.select_arm()
         self.robot.update_actual_pulls(robot_choice)
-        print("robot_choice:", robot_choice)
-        print("robot_choices:", self.robot.actual_pulls)
+        self.robot_history_vector[round_number] = robot_choice
+
         reward = self.bandit.generate_reward(robot_choice)
         self.human.update_rewards(robot_choice, reward)
         self.bandit.update(robot_choice, reward)
 
+        rnn_input = self.human_history_vector + self.robot_history_vector + self.human_policy_vector
+        self.robot.update_train_rnn(rnn_input, reward)
 
-    def simulate(self, rounds):
-        for _ in range(rounds):
-            self.run_round()
+    def simulate(self):
+        for round_number in range(self.n_rounds):
+            self.run_round(round_number)
 
 
 # Example usage
-problem = Problem(4, EpsilonGreedy)
-problem.simulate(1000)
+problem = Problem(4, EpsilonGreedy, n_rounds=1000)
+problem.simulate()
--- robot.py
+++ robot.py
@@ -1,11 +1,16 @@
 import numpy as np
-import
+from rnn import RNN
+import torch
+import torch.nn as nn
 class Robot:
-    def __init__(self, n_arms):
+    def __init__(self, n_arms,input_size):
         self.n_arms = n_arms
         self.human_observations = []
         self.actual_pulls = []
-        self.rnn = None
+        hidden_size = 32
+        self.rnn = RNN(input_size, hidden_size, n_arms)
+        self.optimizer = torch.optim.Adam(self.rnn.parameters())
+        self.criterion = nn.NLLLoss()
 
     def update_human_choice(self, human_choice):
         self.human_observations.append(human_choice)
@@ -14,14 +19,43 @@ class Robot:
         self.actual_pulls.append(robot_pull)
 
 
+    # def select_arm(self):
+    #     # Simple strategy: follow the human's most common choice
+    #     if self.human_observations:
+    #         return np.argmax(np.bincount(self.human_observations))
+    #     return np.random.choice(self.n_arms)
+
     def select_arm(self):
-        # Simple strategy: follow the human's most common choice
-        if self.human_observations:
-            return np.argmax(np.bincount(self.human_observations))
-        return np.random.choice(self.n_arms)
+        # Prepare input tensor from human observations and actual pulls
+        input_tensor = self.prepare_input(self.human_observations, self.actual_pulls)
+        hidden = self.rnn.initHidden()
 
-    def init_rnn(self):
-        self.rnn = RNN(self.n_arms)
+        # Forward pass through the RNN
+        output, hidden = self.rnn(input_tensor, hidden)
 
-    def update_train_rnn(self):
-        pass
+        # Get the probability distribution over arms and sample an arm
+        probabilities = torch.exp(output).detach().numpy().flatten()
+        chosen_arm = np.random.choice(self.n_arms, p=probabilities)
+        return chosen_arm
+
+    def prepare_input(self, human_observations, actual_pulls):
+        # Prepare the input tensor, which could be a one-hot encoded vector
+        # representing the previous choices of human and robot
+        # This is a placeholder and will need to be tailored to your input structure
+        return torch.tensor([human_observations[-1], actual_pulls[-1]], dtype=torch.float).view(1, -1)
+
+    def update_train_rnn(self, rewards):
+        # rewards is a list of the rewards obtained in each round
+        loss = 0
+        hidden = self.rnn.initHidden()
+        for i in range(len(rewards)):
+            input_tensor = self.prepare_input(self.human_observations[i], self.actual_pulls[i])
+            output, hidden = self.rnn(input_tensor, hidden)
+            loss += self.criterion(output, torch.tensor([rewards[i]], dtype=torch.long))
+
+        # Perform backpropagation
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+
+        return loss.item()
